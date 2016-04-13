c66155ff62316668d0346e5d49a4ef23f944afce
Bartosz Szewczyk <Bartosz Szewczyk>
2016-04-13 22:11:49 +0200

    Add MyBaseObject Model and Controller

---
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/MyBaseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SerwisAudiometryczny.Models
+{
+    public class MyBaseModel
+    {
+        public MyBaseModel()
+        {
+            CreationDate = DateTime.Now;
+            Type = "MyBaseModel";
+        }
+        public int ID { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Type { get; set; }
+    }
+    public class MyBaseModelDBContext : DbContext
+    {
+        public MyBaseModelDBContext() : base("DefaultConnection")//Połaczenie z danym connectionstringiem
+        {
+        }
+        public DbSet<MyBaseModel> MyBaseObjects { get; set; }
+    }
+}

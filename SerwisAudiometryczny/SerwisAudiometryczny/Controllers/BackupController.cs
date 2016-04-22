7fe8e2414f34b24ca5f86832e3ac76351762db4c
Bartosz Sowa <b.sowa9@gmail.com>
2016-04-22 18:17:03 +0200

    Dodana klasa MyBackupAndRestore do wykonywania backupow(.bak), BackupController.

---
+++ SerwisAudiometryczny/SerwisAudiometryczny/Controllers/BackupController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Web.Mvc;
+using SerwisAudiometryczny.Models;
+
+namespace SerwisAudiometryczny.Controllers
+{
+    public class BackupController : Controller
+    {
+        MyBackupAndRestore backupAndRestore = new MyBackupAndRestore();
+
+        // GET: Backup
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Backup/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Backup/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Backup/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Backup/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Backup/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Backup/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Backup/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/MyBackupAndRestore.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SerwisAudiometryczny.Models
+{
+    // Backupy jak narazie wykonywane do ścieżki C:\\nazwa.bak
+    public class MyBackupAndRestore
+    {
+        private string databaseName = "SerwisAudiometryczny"; // nazwa bazy danych
+
+        /// <summary>
+        /// Właściwość pobierająca nazwę serwera(Data Source)
+        /// </summary>
+        private string serverName
+        {
+            get
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+                SqlConnectionStringBuilder sqlBuilder = new SqlConnectionStringBuilder(connectionString);
+
+                return sqlBuilder.DataSource;
+            }
+        }
+
+        /// <summary>
+        /// Metoda wykonująca pełną kopię zapasową do pliku
+        /// o nazwie składającej się z aktualnej daty oraz nazwy bazy danych ('yyyy-MM-dd_nazwaBazy.bak')
+        /// </summary>
+        public void BackupDatabase()
+        {
+            Backup sqlBackup = new Backup()
+            {
+                Action = BackupActionType.Database,
+                BackupSetDescription = $"Kopia zapasowa '{databaseName}' wykonana {DateTime.Now.ToShortDateString()}",
+                BackupSetName = "Pełna kopia zapasowa",
+                Database = databaseName
+            };
+            ServerConnection connection = new ServerConnection(serverName);
+            Server sqlServer = new Server(connection);
+            string filename = DateTime.Now.ToString("yyyy-MM-dd") + "_" + databaseName;
+            string dbDir = $"C:\\{filename}.bak";
+            BackupDeviceItem deviceItem = new BackupDeviceItem(dbDir, DeviceType.File);
+
+            sqlServer.ConnectionContext.StatementTimeout = 60;
+            sqlBackup.Devices.Add(deviceItem);
+            sqlBackup.Incremental = false;
+            sqlBackup.Initialize = true;
+
+            try
+            {
+                sqlBackup.SqlBackup(sqlServer);
+            }
+            catch (Exception error)
+            {
+                // Tutaj będzie kod to wyświetlenia komunikatu o błędzie użytkownikowi
+                throw new ApplicationException(error.Message);
+            }
+            sqlBackup.Devices.Remove(deviceItem);
+            connection.Disconnect();
+        }
+
+        /// <summary>
+        /// Metoda wykonująca pełną kopię zapasową do pliku
+        /// o nazwie podanej w parametrze metody.
+        /// </summary>
+        /// <param name="backupFileName">Nazwa kopii zapasowej</param>
+        public void BackupDatabase(string backupFileName)
+        {
+            Backup sqlBackup = new Backup()
+            {
+                Action = BackupActionType.Database,
+                BackupSetDescription = $"Kopia zapasowa '{databaseName}' wykonana {DateTime.Now.ToShortDateString()}",
+                BackupSetName = "Pełna kopia zapasowa",
+                Database = databaseName
+            };
+            ServerConnection connection = new ServerConnection(serverName);
+            Server sqlServer = new Server(connection);
+            string dbDir = $"C:\\{backupFileName}.bak";
+            BackupDeviceItem deviceItem = new BackupDeviceItem(dbDir, DeviceType.File);
+
+            sqlServer.ConnectionContext.StatementTimeout = 60;
+            sqlBackup.Devices.Add(deviceItem);
+            sqlBackup.Incremental = false;
+            sqlBackup.Initialize = true;
+
+            try
+            {
+                sqlBackup.SqlBackup(sqlServer);
+            }
+            catch (Exception error)
+            {
+                // Tutaj będzie kod to wyświetlenia komunikatu o błędzie użytkownikowi
+                throw new ApplicationException(error.Message);
+            }
+            sqlBackup.Devices.Remove(deviceItem);
+            connection.Disconnect();
+        }
+
+        /// <summary>
+        /// Metoda wykonująca przywracanie bazy danych z kopii zapasowej.
+        /// </summary>
+        /// <param name="backupFileName">Nazwa kopii zapasowej</param>
+        public void RestoreDatabase(string backupFileName)
+        {
+            Restore sqlRestore = new Restore()
+            {
+                Action = RestoreActionType.Database,
+                Database = databaseName,
+                ReplaceDatabase = true
+            };
+            ServerConnection connection = new ServerConnection(serverName);
+            Server sqlServer = new Server(connection);
+            BackupDeviceItem deviceItem = new BackupDeviceItem($"C:\\{backupFileName}.bak", DeviceType.File);
+
+            sqlServer.ConnectionContext.StatementTimeout = 60;
+            sqlRestore.Devices.Add(deviceItem);
+            sqlServer.KillAllProcesses(databaseName); //trzeba zakończyć wszystkie połączenia z bazą, aby ją przywrócić
+            try
+            {
+                sqlRestore.SqlRestore(sqlServer);
+            }
+            catch (Exception error)
+            {
+                // Tutaj będzie kod to wyświetlenia komunikatu o błędzie użytkownikowi
+                throw new ApplicationException(error.Message);
+            }
+            sqlRestore.Devices.Remove(deviceItem);
+            connection.Disconnect();
+        }
+    }
+
+}

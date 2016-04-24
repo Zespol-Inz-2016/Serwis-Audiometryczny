9943ec21d3305df25b22157f9de5e32e4efc43f5
KapibaraPiotr <kapibarapiotr@interia.pl>
2016-04-24 21:32:48 +0200

    Dodano Modele Audiogramów i Punktów na audiogramie.

---
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/AudiogramModel.cs
@@ -0,0 +1,27 @@
+﻿using SerwisAudiometryczny.Models;
+using SerwisAudiometryczny.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SerwisAudiometryczny.Models
+{
+    public class AudiogramModel : MyBaseModel, IMyBase
+    {
+        public AudiogramModel() : base()
+        {
+            Type = "Audiogram";
+        }
+        public int PatientID { get; set; }
+        public PointModel[] Points { get; set; }
+        public int DictDiagnosisModelID { get; set; }
+        public int DictSexModelID { get; set; }
+        public int DictNuisanceModelID { get; set; }
+        public int DictInstrumentModelID { get; set; }
+        public bool IsMusican { get; set; }
+        public float PercentageHearingLoss { get; set; }
+        public int PatientAge { get; set; }
+    }
+}
---
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/PointModel.cs
@@ -0,0 +1,21 @@
+﻿using SerwisAudiometryczny.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SerwisAudiometryczny.Models
+{
+    public class PointModel : MyBaseModel, IMyBase
+    {
+        public PointModel() : base()
+        {
+            Type = "PointModel";
+        }
+        public int AudiogramID { get; set; }
+        public int Frequency { get; set; }
+        public float HearingThresholdLevel { get; set; }
+
+    }
+}

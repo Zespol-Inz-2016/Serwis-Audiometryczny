b7c8572ae525f799f3ef790785a04c01f2f4498f
Paweł Wasil <pawel.hr@gmail.com>
2016-05-04 23:40:48 +0200

    Dodanie pakietu PagedList.mvc umożliwiającego stronicowanie logów, dodanie wstępnego kontrolera i widoków oraz poprawa komentarzy w LogSearchViewModel

--- SerwisAudiometryczny/SerwisAudiometryczny/Models/LogSearchViewModel.cs
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/LogSearchViewModel.cs
@@ -11,27 +11,27 @@ namespace SerwisAudiometryczny.Models
     public class LogSearchViewModel
     {
         /// <summary>
-        /// IdUser - zmienna przechowywująca numer indetyfikacyjny użytkownika, którego chcemy wyszukać w logach
+        /// Zmienna przechowywująca numer indetyfikacyjny użytkownika, którego chcemy wyszukać w logach
         /// </summary>
         public int? IdUser { get; set; }
 
         /// <summary>
-        /// Controller - zmienna przechowywująca miejsce, które chcemy wyszukać w logach
+        /// Zmienna przechowywująca miejsce, które chcemy wyszukać w logach 
         /// </summary>
         public string Controller { get; set; }
 
         /// <summary>
-        /// Action - zmienna przechowywująca akcję, którą chcemy wyszukać w logach
+        /// Zmienna przechowywująca akcję, którą chcemy wyszukać w logach 
         /// </summary>
         public string Action { get; set; }
 
         /// <summary>
-        /// FromTime - zmienna przechowywująca czas od kiedy chcemy wyszukać w logach
+        /// Zmienna przechowywująca czas od kiedy chcemy wyszukać w logach 
         /// </summary>
         public DateTime FromTime { get; set; }
 
         /// <summary>
-        /// ToTime - zmienna przechowywująca czas do kiedy chcemy wyszukać w logach
+        /// Zmienna przechowywująca czas do kiedy chcemy wyszukać w logach 
         /// </summary>
         public DateTime ToTime { get; set; }
 

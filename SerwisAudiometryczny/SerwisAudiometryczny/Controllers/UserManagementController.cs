f6d34dd2d982c04226a2fbbe0416f70e5a447dd3
Bartosz Sowa <b.sowa9@gmail.com>
2016-05-14 16:13:16 +0200

    Dodanie szkieletu kontrolera UserManagementController.

---
+++ SerwisAudiometryczny/SerwisAudiometryczny/Controllers/UserManagementController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SerwisAudiometryczny.Models;
+
+namespace SerwisAudiometryczny.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public ApplicationUserManager UserManager;
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        public ActionResult CreateUser(UserCreateModelView model)
+        {
+            return View();
+        }
+
+        public ActionResult DeactivateUser(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult DeactivateUserConfirmed(int id)
+        {
+            return View();
+        }
+
+        public ActionResult EditUser(int? id)
+        {
+            return View();
+        }
+
+        public ActionResult EditUser(UserEditModelView model)
+        {
+            return View();
+        }
+    }
+}

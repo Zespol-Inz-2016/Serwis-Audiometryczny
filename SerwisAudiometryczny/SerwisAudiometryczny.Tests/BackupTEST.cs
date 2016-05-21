fb6774922de6c1fc07d6c86a0fc8e31a71bb984e
Krzysiek <Krzysiek@KRZYSIEKPC>
2016-05-22 00:16:03 +0200

    Dodałem pliczek testowy z backupu misie <3

---
+++ SerwisAudiometryczny/SerwisAudiometryczny.Tests/BackupTEST.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerwisAudiometryczny;
+using SerwisAudiometryczny.Controllers;
+using SerwisAudiometryczny.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SerwisAudiometryczny.Tests
+{
+    [TestClass]
+    public class BackupTEST
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

6deea6092d4386f59db33c535bd9c982da8c50c6
KapibaraPiotr <kapibarapiotr@interia.pl>
2016-05-29 23:27:39 +0200

    Dodanie modeli AudiogramCreateEditViewModel i AudiogramDisplayViewModel do przekazywania modeli do widoków.

---
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/AudiogramCreateEditViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SerwisAudiometryczny.Models
+{
+    /// <summary>
+    /// Służy przekazaniu wszystkich potrzebnych modeli do widoku edycji.
+    /// </summary>
+    public class AudiogramCreateEditViewModel
+    {
+        /// <summary>
+        /// Służy przekazaniu modelu audiogramu do i z widoku edycji.
+        /// </summary>
+        public AudiogramModel Audiogram { get; set; }
+        /// <summary>
+        /// Służy przekazaniu tablicy częstotliwości do i z widoku edycji.
+        /// </summary>
+        public int[] Frequencies { get; set; }
+        /// <summary>
+        /// Służy przekazaniu listy instrumentów do i z widoku edycji.
+        /// </summary>
+        public List<InstrumentModel> Instruments { get; set; }
+        /// <summary>
+        /// Służy przekazaniu nowego instrumentu z widoku edycji.
+        /// </summary>
+        public InstrumentModel NewInstrument { get; set; }
+    }
+}
---
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/AudiogramDisplayViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SerwisAudiometryczny.Models
+{        
+    /// <summary>
+    /// Służy przekazaniu wszystkich potrzebnych modeli do widoku wyświetlania.
+    /// </summary>
+    public class AudiogramDisplayViewModel
+    {
+        /// <summary>
+        /// Służy przekazaniu modelu audiogramu do widoku wyświetlania.
+        /// </summary>
+        public AudiogramModel Audiogram { get; set; }
+        /// <summary>
+        /// Służy przekazaniu modelu Pacjenta do widoku wyświetlania.
+        /// </summary>
+        public ApplicationUser Patient { get; set; }
+        /// <summary>
+        /// Służy przekazaniu modelu Edytora do widoku wyświetlania.
+        /// </summary>
+        public ApplicationUser Editor { get; set; }
+        /// <summary>
+        /// Służy przekazaniu tablicy częstotliwości do widoku wyświetlania.
+        /// </summary>
+        public int[] Frequencies { get; set; }
+    }
+}

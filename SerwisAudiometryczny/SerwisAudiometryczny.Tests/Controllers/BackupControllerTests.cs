cfe86c142266e2df357596349344975afbc0ecae
Krisow15 <ksobiech15@gmail.com>
2016-05-30 11:34:59 +0200

    Add files via upload
    BackupControllerTest

---
+++ SerwisAudiometryczny/SerwisAudiometryczny.Tests/Controllers/BackupControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SerwisAudiometryczny.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerwisAudiometryczny.Controllers.Tests
+{
+    [TestClass()]
+    public class BackupControllerTests
+    {
+        [TestMethod()]
+        public void ImportTest()
+        {
+           
+        }
+
+        [TestMethod()]
+        public void ExportTest()
+        {
+           
+        }
+
+        [TestMethod()]
+        public void SendFileTest()
+        {
+         
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+           
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+           
+        }
+
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+           
+        }
+
+    }
+}

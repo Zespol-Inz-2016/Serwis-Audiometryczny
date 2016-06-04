4103a3daa8cf5d5fe8e3a3113a29fcb333ff5769
Bartosz Sowa <b.sowa9@gmail.com>
2016-06-04 16:06:07 +0200

    Usunięcie niektórych testów Backup-ow po zmianie BackupControllera.

--- SerwisAudiometryczny/SerwisAudiometryczny.Tests/Controllers/BackupControllerTests.cs
+++ SerwisAudiometryczny/SerwisAudiometryczny.Tests/Controllers/BackupControllerTests.cs
@@ -49,58 +49,5 @@ namespace SerwisAudiometryczny.Controllers.Tests
 
             Assert.AreSame(oczekiwany, aktualny);
         }
-
-        [TestMethod()]
-        public void CreateTest(FormCollection collection)
-        {
-            BackupController TestCreate = new BackupController();
-            ViewResult wynik = TestCreate.Create(collection) as ViewResult;
-
-            Assert.IsNotNull(wynik);
-
-        }
-
-        [TestMethod()]
-        public void EditTest1()
-        {
-            BackupController TestEdit = new BackupController();
-            int PodaneID = 2; //należy podać ID 
-            ViewResult wynik = TestEdit.Edit(PodaneID) as ViewResult;
-
-            Assert.IsNotNull(wynik);
-        }
-
-        [TestMethod()]
-        public void EditTest2(FormCollection collect)
-        {
-            BackupController TestEdit = new BackupController();
-            int PodaneID = 2; //należy podać ID 
-            ViewResult wynik = TestEdit.Edit(PodaneID,collect) as ViewResult;
-
-            Assert.IsNotNull(wynik);
-        }
-
-
-        [TestMethod()]
-        public void DeleteTest1()
-        {
-            BackupController TestDelete = new BackupController();
-            int PodaneID = 5;
-            ViewResult wynik = TestDelete.Delete(PodaneID) as ViewResult;
-
-            Assert.IsNotNull(wynik);
-
-        }
-
-        [TestMethod()]
-        public void DeleteTest2(FormCollection collect)
-        {
-            BackupController TestDelete = new BackupController();
-            int PodaneID = 3;
-            ViewResult wynik = TestDelete.Delete(PodaneID, collect) as ViewResult;
-
-            Assert.IsNotNull(wynik);
-
-        }
     }
 }

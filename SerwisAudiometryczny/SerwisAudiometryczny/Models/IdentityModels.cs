9c42a2d39274aa924429b7ea7072a5a8d3830732
Bartosz Sowa <b.sowa9@gmail.com>
2016-06-05 15:25:38 +0200

    Zmiana nazwy InstrumentViews na InstrumentsViews (wcześniej niespójne z nazwą kontrolera)  + drobna dokumetancja.

--- SerwisAudiometryczny/SerwisAudiometryczny/Models/IdentityModels.cs
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/IdentityModels.cs
@@ -15,8 +15,8 @@ namespace SerwisAudiometryczny.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser<int, CustomUserLogin, CustomUserRole, CustomUserClaim>, IXmlSerializable
     {
-        private ModelsDbContext dbContext;
-        private ApplicationDbContext applicationDbContext;
+        // private ModelsDbContext dbContext;
+        // private ApplicationDbContext applicationDbContext;
 
         /// <summary>
         /// Rola administratora.
@@ -80,6 +80,11 @@ namespace SerwisAudiometryczny.Models
             return userIdentity;
         }
 
+        /// <summary>
+        /// Wymagana przez interfejs IXmlSerializable, ale nie zalecana 
+        /// w impelementacji własnej.
+        /// </summary>
+        /// <returns></returns>
         public XmlSchema GetSchema()
         {
             return (null);

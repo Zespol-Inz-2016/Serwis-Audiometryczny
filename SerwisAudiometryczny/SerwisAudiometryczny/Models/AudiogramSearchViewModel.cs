70792c544e4789d77cd11e4bda11a0be58d7f8c0
Bartosz Sowa <b.sowa9@gmail.com>
2016-06-13 13:49:37 +0200

    Poprawki dokumentacji modeli + usunięcie zbędnych przestrzeni klas w modelach.

--- SerwisAudiometryczny/SerwisAudiometryczny/Models/AudiogramSearchViewModel.cs
+++ SerwisAudiometryczny/SerwisAudiometryczny/Models/AudiogramSearchViewModel.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SerwisAudiometryczny.Models
+﻿namespace SerwisAudiometryczny.Models
 {
+    /// <summary>
+    /// Służy przekazaniu wszystkich potrzebnych informacji o audiogramach do widoku wyszukiwarki.
+    /// </summary>
     public class AudiogramSearchViewModel
     {
         /// <summary>
